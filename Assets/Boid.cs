e42cd3da81619b5083c80eb3d7f9d67cb8491e49
ryangg87 <ryan.goodman878@gmail.com>
2025-05-14 10:53:04 -0400

    fish movign and change cloors

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -2,4 +2,20 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
+    public Rigidbody ri;
+    private void Awake()
+    {
+
+       ri = GetComponent<Rigidbody>();
+       ri.linearVelocity = Random.insideUnitSphere;
+        GetComponent<Renderer>().material.SetColor("_basecolor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
+
+    }
+    public void asingtovolicity()
+    {
+
+
+
+        transform.forward = Vector3.RotateTowards(transform.forward, ri.linearVelocity.normalized, Mathf.Deg2Rad * 1800 * Time.deltaTime, 100);
+     }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -2,5 +2,58 @@ using UnityEngine;
 
 public class BoidSimulationControl : MonoBehaviour
 {
+    
+   public enum ControlMode
+    {
+         
+        seek,
+        pursue,
+        food,
+        obstacle
 
+    }
+
+    public ControlMode controlMode = ControlMode.seek;
+    public int controlModeInt = 0;
+    public GameObject boidprefab = null;
+    public int numBoidsTospawn = 10;
+  
+    public void Start()
+    {
+
+       
+        for (int i = 0; i < numBoidsTospawn; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-1.4f, 0.9f), Random.Range(0f, 1.4f), Random.Range(-0.9f, 0.9f));
+            Quaternion rotation = Random.rotation;
+            GameObject spawndBoid = Instantiate(boidprefab, position, rotation);
+            spawndBoid.transform.localScale *= Random.Range(0.9f, 3f);
+            spawndBoid.GetComponent<Renderer>().material.SetColor("_basecolor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
+           
+
+
+        }
+    }
+    public void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            controlMode = ControlMode.seek;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            controlMode = ControlMode.pursue;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            controlMode = ControlMode.food;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            controlMode = ControlMode.obstacle;
+        }
+    }
+    
 }
