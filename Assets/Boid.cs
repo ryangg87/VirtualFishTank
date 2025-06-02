1ec7e840e7dbd9c34c5411950b52aa3c08192c09
ryangg87 <101553622@georgebrown.ca>
2025-06-02 16:55:52 -0400

    add

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
+    
     public Rigidbody ri;
+    public Vector3 velocity;
+
+    public float accelerationRate = 1f;
+    public float maxSpeed = 10f;
+    public float acclmax = 5f;
+
+
+    public float desiredsoeed = 5;
+
     private void Awake()
     {
 
@@ -18,4 +28,64 @@ public class Boid : MonoBehaviour
 
         transform.forward = Vector3.RotateTowards(transform.forward, ri.linearVelocity.normalized, Mathf.Deg2Rad * 1800 * Time.deltaTime, 100);
      }
+    //
+    public Vector3 seek(Vector3 target,float acceleration)
+    {
+
+        
+        Vector3 toTarget = target - transform.position;
+
+        Vector3 totargetNormalized = toTarget.normalized;
+        Vector3 accel = totargetNormalized * acceleration;
+
+        return accel;
+    }
+    public Vector3 pUrsue(Vector3 target, float acceleration,float desiredsoeed)
+    {
+
+        //taget is the postion of the mosue tranfor psot get the postion of the fish
+        //to toaget is the displamcet 
+        Vector3 toTarget = target - transform.position;
+        //to tgaget.
+        //sam vector but with a leanght of 1
+        Vector3 totargetNormalized = toTarget.normalized;
+        //disrespped is the dircation and the spped
+
+        Vector3 desiredVolocity = totargetNormalized * desiredsoeed;
+        //how much you want to change your volite //devlat is the difrice in volcitys riquel
+        //wher i wan tto be goign - whern i am goign 
+        Vector3 deltavel = desiredVolocity - ri.linearVelocity;
+        //acle give the rate at what you acllrate actacion give mangutin .normald gie us driaction with out mangutes 
+        Vector3 accel = deltavel.normalized * acceleration;
+
+
+
+        return accel;
+    }
+    //
+
+    public void Update()
+    {
+
+
+    }
+    public void FixedUpdate()
+    {
+        float speed = ri.linearVelocity.magnitude;
+        if(speed> maxSpeed)
+        {
+            ri.linearVelocity = ri.linearVelocity * maxSpeed / speed;
+        }
+        asingtovolicity();
+    }
+
+    internal Vector3 seek(Vector3 position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    internal Vector3 pUrsue(Vector3 position, float acclmax, object desiredsoeed)
+    {
+        throw new System.NotImplementedException();
+    }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 
+
 public class BoidSimulationControl : MonoBehaviour
 {
-    
-   public enum ControlMode
+    //public Rigidbody rigibody;
+    //public Vector3 velocity;
+    //public float accelerationRate = 1f;
+    // public float maxSpeed = 10f;
+    //public float acclmax = 5f;
+    // public float linercolvity;
+
+    public enum ControlMode
     {
-         
+
         seek,
         pursue,
         food,
@@ -17,11 +24,22 @@ public class BoidSimulationControl : MonoBehaviour
     public int controlModeInt = 0;
     public GameObject boidprefab = null;
     public int numBoidsTospawn = 10;
-  
+    public Boid[] boids = new Boid[10];
+    // Rigidbody targetObject;
+    GameObject targetObject;
     public void Start()
     {
+        //
 
-       
+        targetObject = GameObject.Find("target");
+
+        //
+
+
+        boids = new Boid[10];
+
+
+        //
         for (int i = 0; i < numBoidsTospawn; i++)
         {
             Vector3 position = new Vector3(Random.Range(-1.4f, 0.9f), Random.Range(0f, 1.4f), Random.Range(-0.9f, 0.9f));
@@ -29,31 +47,132 @@ public class BoidSimulationControl : MonoBehaviour
             GameObject spawndBoid = Instantiate(boidprefab, position, rotation);
             spawndBoid.transform.localScale *= Random.Range(0.9f, 3f);
             spawndBoid.GetComponent<Renderer>().material.SetColor("_basecolor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
-           
 
+            Boid boidcompent = spawndBoid.GetComponent<Boid>();
+            boidcompent.maxSpeed = Random.Range(0.5f, 1.5f);
+            boidcompent.acclmax = Random.Range(0.5f, 1.5f);
 
+            boids[i] = boidcompent;
         }
+
+
+
+
+
     }
-    public void Update()
+    //
+    private void FixedUpdate()
     {
 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        bool didhit = Physics.Raycast(ray, out hitInfo, 100);
+        if (didhit)
+        {
+            targetObject.transform.position = hitInfo.point;
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            controlMode = ControlMode.seek;
+
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            controlMode = ControlMode.pursue;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            controlMode = ControlMode.food;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            controlMode = ControlMode.obstacle;
-        }
+
     }
-    
+    //
+
+
+    public void Update()
+    {
+        /*
+        // if (Input.GetButton(1)) ;
+        {
+            for (int i = 0; i < numBoidsTospawn; i++)
+            {
+                Vector3 accel = boids[i].seek(targetObject.transform.position, boids[i].acclmax);
+
+                if (Input.GetMouseButton(0))
+                {
+                    boids[i].ri.linearVelocity += accel * Time.fixedDeltaTime;
+                    Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+                }
+                //  boids[i].ri.linearVelocity += accel * Time.fixedDeltaTime;
+                // Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+                // boids[i].ri.linearVelocity -= accel * Time.fixedDeltaTime;
+                // Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+                else if (Input.GetMouseButton(1))
+                {
+                    boids[i].ri.linearVelocity -= accel * Time.fixedDeltaTime;
+                    Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+
+                }
+
+
+
+            }
+        }
+
+
+        */
+        //if (purse == true)
+        
+
+            for (int i = 0; i < numBoidsTospawn; i++)
+            {
+                Vector3 accel = boids[i].pUrsue(targetObject.transform.position, boids[i].acclmax, boids[i].desiredsoeed);
+
+                if (Input.GetMouseButton(0))
+                {
+                    boids[i].ri.linearVelocity += accel * Time.fixedDeltaTime;
+                    Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+                }
+                //  boids[i].ri.linearVelocity += accel * Time.fixedDeltaTime;
+                // Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+                // boids[i].ri.linearVelocity -= accel * Time.fixedDeltaTime;
+                // Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+                else if (Input.GetMouseButton(1))
+                {
+                    boids[i].ri.linearVelocity -= accel * Time.fixedDeltaTime;
+                    Debug.DrawRay(boids[i].transform.position, accel, Color.green);
+
+                }
+
+            }
+        
+
+             
+
+            // float dt = Time.deltaTime;
+            // Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            // Vector3 acceleration = input.normalized * accelerationRate;
+            // velocity += acceleration * dt;
+            // velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            //linercolvity = velocity.y;
+            //Vector3 linearVelocity = rigibody.linearVelocity;
+            // transform.position += velocity * dt;
+
+            //
+
+
+            //  Debug.DrawRay(transform.position, rigibody.linearVelocity, Color.red);
+            //
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                controlMode = ControlMode.seek;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                controlMode = ControlMode.pursue;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                controlMode = ControlMode.food;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                controlMode = ControlMode.obstacle;
+            }
+
+
+
+        
+
+
+    }
 }
